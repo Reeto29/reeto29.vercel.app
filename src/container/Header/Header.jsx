1e78a6b9655ead5e3c5c8e5a7b6cc55125881137
reetog <reetog29@gmail.com>
2022-12-30 22:18:22 -0500

    header and navbar

---
+++ src/container/Header/Header.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+
+import {images} from '../../constants';
+import './Header.css';
+
+const Header = () => (
+  <div className="app__header app__wrapper section__padding" id="home">
+      <img className='image__style' src={images.mylogo} alt="header img"></img>
+      <div>
+
+        <h1 className='app__header-h1'>Hi! My Name is Reeto.</h1>
+        <p className= 'subheading'style={{margin: '2rem 0'}}>
+          I am a first year student in Waterloos Honours Mathematics program.
+        </p>
+
+        <div className='button__links'>
+            <form action="https://github.com/Reeto29" target="_blank"> 
+              <button className='buttonImage' type="submit">
+                <img className="image__styling" src={images.github} alt="GitHub"/>
+              </button> 
+            </form> 
+
+            <form action="https://www.instagram.com/ldh_codeclub/" target="_blank"> 
+            <button className='buttonImage' type="submit">
+                <img className="image__styling" src={images.instagram} alt="Instagram"/>
+              </button> 
+            </form> 
+
+            <form action="https://www.linkedin.com/in/reetoghosh/" target="_blank"> 
+            <button className='buttonImage' type="submit">
+                <img className="image__styling" src={images.linkedin} alt="Linkedin"/>
+              </button> 
+          </form> 
+        </div>
+      </div>
+
+  </div>
+  
+);
+
+export default Header;
---
+++ src/container/Projects/Projects.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { images } from "../../constants";
+import './Projects.css';
+
+const Projects = () => (
+  <div className='app__projects section__padding' id="Projects">
+      <div className="projectBox">
+        <p className ="textStyle">Basketball Bot</p>
+
+      </div>
+  </div>
+);
+
+export default Projects;
